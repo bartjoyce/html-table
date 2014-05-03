4e9279944e2ad4ee42eb705d5e7cf17f4ec61ded
bartjoyce <bartholomew.michael.joyce@gmail.com>
2014-05-03 15:56:10 +0100

    Removed multi-selection capabilities

--- src/components/click_select.js
+++ src/components/click_select.js
@@ -2,46 +2,25 @@
 (function() {
   'use strict';
 
-  var SHIFT_KEY = 16;
-
   var tableElements = [],
-      selectedFields = [],
-      holdField = false;
+      selectedField = null;
 
-  /* Table deselection */
-  var deselectAll = function deselectAll() {
-    while (selectedFields.length > 0)
-      deselectElement(selectedFields[0]);
-  };
-
-  var deselectElement = function deselectElement(element) {
-    if (element.classList.contains('selected')) {
-      element.classList.remove('selected');
-
-      var index = selectedFields.indexOf(element);
-      if (index !== -1)
-        selectedFields.splice(index, 1);
-    } else
-      deselect(element);
-  };
-
-  /* Field selection */
+  /* Field selection/deselection */
   var selectField = function selectField(fieldElement) {
-    if (!holdField)
-      deselectAll();
-    else if (fieldElement.classList.contains('selected')) {
-      deselectElement(fieldElement);
-      selectionChangeEvent.trigger(selectedFields);
-      return;
-    }
+    deselectField();
 
     fieldElement.classList.add('selected');
+    selectedField = fieldElement;
 
-    if (selectedFields.indexOf(fieldElement) === -1)
-      selectedFields.push(fieldElement);
-
-    selectionChangeEvent.trigger(selectedFields);
+    selectionChangeEvent.trigger(selectedField);
   }
+
+  var deselectField = function deselectField() {
+    if (selectedField !== null) {
+      selectedField.classList.remove('selected');
+      selectedField = null;
+    }
+  };
 
   /* Click event */
   var fieldOnClick = function fieldOnClick(e) {
@@ -49,30 +28,11 @@
     e.stopPropagation();
   };
 
-  /* Key-press events */
-  var windows = [];
-  var windowAttachEvents = function windowAttachEvents(element) {
-    var win = element.ownerDocument.defaultView || element.ownerDocument.parentWindow;
-    if (windows.indexOf(win) === -1) {
-      win.addEventListener('keydown', windowOnKeyDown, false);
-      win.addEventListener('keyup', windowOnKeyUp, false);
-      windows.push(win);
-    }
-  };
-  var windowOnKeyDown = function windowOnKeyDown(e) {
-    holdField = (e.keyCode === SHIFT_KEY) ? true : holdField;
-  };
-  var windowOnKeyUp = function windowOnKeyUp(e) {
-    holdField = (e.keyCode === SHIFT_KEY) ? false : holdField;
-  };
-
   /* Register components */
   var clickSelectTableComponent = new Table.Component('click_select_table', ['table'], {},
     function(tableElement) {
       if (tableElements.indexOf(tableElement) === -1)
         tableElements.push(tableElement);
-
-      windowAttachEvents(tableElement);
   });
 
   var clickSelectComponent = new Table.Component('click_select', ['field'], {
@@ -84,13 +44,12 @@
 
   Table.clickSelect = {
     selectField: selectField,
-    deselectAll: deselectAll,
-    deselect: deselectElement
+    deselect: deselectField
   };
 
   /* Updating support */
   Table.addEventListener('update', function(element) {
     if (Table.util.getElementType(element) === 'table')
-      deselectAll();
+      deselect();
   });
 })();
