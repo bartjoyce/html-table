da1504077d2a0e3ac30991061da40a4dfad7671d
bartjoyce <bartholomew.michael.joyce@gmail.com>
2014-05-03 16:01:27 +0100

    Added comments; deselects table on blur

--- demo/script.js
+++ demo/script.js
@@ -1,3 +1,10 @@
 window.addEventListener('load', function() {
+  // Initiate the table handling script on the current window
   Table.load(window);
+
+  // Deselect table(s) when clicking away
+  document.addEventListener('click', function() {
+    // The click_select component has its exported methods in Table.clickSelect
+    Table.clickSelect.deselect();
+  });
 });
